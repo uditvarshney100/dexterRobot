56f0fe0afe5938e6e6243927fb74f76eb82b273a
InukaAsith <inukaasith7@gmail.com>
2021-07-20 16:31:24 +0000

    Auto Fixes: Code formatted

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -85,7 +85,9 @@ async def get_start_func(message, strings, edit=False):
         ),
     )
     buttons.add(
-        InlineKeyboardButton(strings["btn_channel"], url="https://t.me/DexterRobotNews"),
+        InlineKeyboardButton(
+            strings["btn_channel"], url="https://t.me/DexterRobotNews"
+        ),
         InlineKeyboardButton(
             strings["btn_group"], url="https://t.me/DaisySupport_Official"
         ),
